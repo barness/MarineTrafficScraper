0756fdace4d1e62432a07ea296bf40fbbe957a43
Nate Devine <ndevine@conncoll.edu>
2018-10-07 23:59:03 -0400

    added xpaths for login and password fields, added basic login logic

--- marinetraffic.py
+++ marinetraffic.py
@@ -8,8 +8,19 @@ class MarineTrafficSpider(SixGillSpider):
     name = "marinetraffic"
     allowed_domains = [u'www.marinetraffic.com']
     main_url = u'https://www.marinetraffic.com/en/ais/details/ships/shipid:442329/vessel:NORTHWESTERN'
+    
+    username = 'ndevine@conncoll.edu'
+    password = '7gFh0JI1KM0hF0H5qxHfJnGLwmEz17y'
 
     force_tor = False
+    
+    ###
+    username_login_xpath = '//input[@id="email"]'
+    password_login_xpath = '//input[@id="password"]'
+    submit_login_xpath = '//button[@type="submit"]'
+    bad_credentials_xpath = '//div[@id="login-alert"]'
+    logout_xpath = '//span[contains(text,"Logout")]'
+    ###
 
     imo_xpath = u'html/body/main/div/div/div[1]/div[5]/div[1]/div[1]/div/div[1]/div[1]/b/node()'
     mmsi_xpath = u'html/body/main/div/div/div[1]/div[5]/div[1]/div[1]/div/div[1]/div[2]/b/node()'
@@ -29,14 +40,17 @@ class MarineTrafficSpider(SixGillSpider):
     latitude_longitude_xpath = u".//*[@id='tabs-last-pos']/div/div/div[1]/div[4]/span[2]/strong/a/text()"
     position_status_xpath = u".//*[@id='tabs-last-pos']/div/div/div[1]/div[5]/span[2]/strong/text()"
     speed_xpath = u".//*[@id='tabs-last-pos']/div/div/div[1]/div[6]/span[2]/strong/text()"
-
-
+    
     def start_requests(self, **kwargs):
         return super(MarineTrafficSpider, self).start_requests(
-            self.main_url,
-            [],
-            ** kwargs
+            self.login_url,
+            [{'page_indicator': self.username_login_xpath,
+              'items':
+                  [
+                      InputField(self.username_login_xpath, self.username),
+                      InputField(self.password_login_xpath, self.password),
+                      ClickField(self.submit_login_xpath),
+                      BadCredentialsElement(self.bad_credentials_xpath)
+                  ]
+              }]
         )
-    
-    def _is_user_in_body(self, sel):
-        return True
