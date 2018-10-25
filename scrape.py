4ce2efed5d0bac29e7a6b83cbe58abee07a65135
Nate Devine <ndevine@conncoll.edu>
2018-10-24 21:19:36 -0400

    added scrape.py

---
+++ scrape.py
@@ -0,0 +1,21 @@
+import requests
+from BeautifulSoup import BeautifulSoup
+
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+helpful_link = 'en/ais/details/ships/shipid:442329/vessel:NORTHWESTERN'
+
+def scrapeURL(url):
+        
+    main_url = 'https://www.marinetraffic.com/'
+    my_url = main_url + url
+    
+    response = requests.get(my_url, headers=headers)
+    html = response.content
+
+    #BeautifulSoup(markup, "xml")
+    soup = BeautifulSoup(html, "xml")
+    
+    #print(soup.title.string)
+    print("test")
+    
+scrapeURL(helpful_link)
